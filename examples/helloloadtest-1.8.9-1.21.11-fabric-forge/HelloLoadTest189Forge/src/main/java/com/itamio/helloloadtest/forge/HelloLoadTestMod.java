3f2138cce8cc66541e5460483ae5aae156a9c86a
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-03-09 20:10:22 +0800

    Add hello load smoke test batch

---
+++ examples/helloloadtest-1.8.9-1.21.11-fabric-forge/HelloLoadTest189Forge/src/main/java/com/itamio/helloloadtest/forge/HelloLoadTestMod.java
@@ -0,0 +1,16 @@
+package com.itamio.helloloadtest.forge;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = HelloLoadTestMod.MODID, version = HelloLoadTestMod.VERSION)
+public class HelloLoadTestMod {
+    public static final String MODID = "helloloadtest";
+    public static final String VERSION = "1.0.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("[Hello Load Test] Forge 1.8.9 loaded.");
+    }
+}
