bfebc2c7129213e3c1f2c414df06502d73803685
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-03-09 19:16:15 +0800

    Use manual event bus registration for PingFix Forge 1.21

--- examples/pingfix-failed-rerun/PingFix121ForgeMid/src/main/java/com/itamio/pingfix/forge/PingFixForgeClient.java
+++ examples/pingfix-failed-rerun/PingFix121ForgeMid/src/main/java/com/itamio/pingfix/forge/PingFixForgeClient.java
@@ -1,24 +1,18 @@
 package com.itamio.pingfix.forge;
 
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(
-    modid = PingFixForgeMod.MOD_ID,
-    bus = Mod.EventBusSubscriber.Bus.FORGE,
-    value = Dist.CLIENT
-)
 public final class PingFixForgeClient {
     private static final PingFixController CONTROLLER = new PingFixController();
 
     private PingFixForgeClient() {
     }
 
-    @SubscribeEvent
-    public static void onClientTick(TickEvent.ClientTickEvent.Post event) {
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) {
+            return;
+        }
         CONTROLLER.onClientTick(Minecraft.getInstance());
     }
 }
