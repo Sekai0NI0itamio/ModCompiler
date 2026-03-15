2a039866fe05592857a52fd5748e6b49eb3448c2
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-03-15 08:08:16 +0800

    after ai changes

--- 1.21.2-1.21.8/forge/template/src/main/java/com/example/examplemod/Config.java
+++ 1.21.2-1.21.8/forge/template/src/main/java/com/example/examplemod/Config.java
@@ -1,13 +1,15 @@
 package com.example.examplemod;
 
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.Objects;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,7 +17,8 @@ import java.util.stream.Collectors;
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config {
+public class Config
+{
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
@@ -42,19 +45,35 @@ public class Config {
     public static String magicNumberIntroduction;
     public static Set<Item> items;
 
-    private static boolean validateItemName(final Object obj) {
-        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(Identifier.tryParse(itemName));
+    private static ResourceLocation parseItemName(String name) {
+        int sep = name.indexOf(':');
+        if (sep > 0 && sep < name.length() - 1) {
+            return ResourceLocation.fromNamespaceAndPath(name.substring(0, sep), name.substring(sep + 1));
+        }
+        return ResourceLocation.fromNamespaceAndPath("minecraft", name);
+    }
+
+    private static boolean validateItemName(final Object obj)
+    {
+        if (!(obj instanceof String)) {
+            return false;
+        }
+        return ForgeRegistries.ITEMS.containsKey(parseItemName((String) obj));
     }
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
+    static void onLoad(final ModConfigEvent event)
+    {
         logDirtBlock = LOG_DIRT_BLOCK.get();
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
 
         // convert the list of strings into a set of items
         items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(Identifier.tryParse(itemName)))
+                .map(String::valueOf)
+                .map(Config::parseItemName)
+                .map(ForgeRegistries.ITEMS::getValue)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toSet());
     }
 }
