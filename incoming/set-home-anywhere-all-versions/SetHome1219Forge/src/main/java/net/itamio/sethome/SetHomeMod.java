3c90e6ad3149db4d4dde824231c2dffc4726b17a
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-19 11:51:17 +0800

    Fix Forge 1.21.2+: Use SavedDataType as top-level class

--- incoming/set-home-anywhere-all-versions/SetHome1219Forge/src/main/java/net/itamio/sethome/SetHomeMod.java
+++ incoming/set-home-anywhere-all-versions/SetHome1219Forge/src/main/java/net/itamio/sethome/SetHomeMod.java
@@ -5,6 +5,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.saveddata.SavedDataType;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -82,12 +83,13 @@ public class SetHomeMod {
 
     public static class HomeData extends SavedData {
         private static final String NAME = "sethome_data";
+        private static final SavedDataType<HomeData> TYPE = new SavedDataType<>(HomeData::new, (tag, provider) -> HomeData.load(tag), null);
         private final Map<String, Map<String, double[]>> data = new HashMap<>();
         public HomeData() {}
 
         public static HomeData get(MinecraftServer srv) {
             DimensionDataStorage storage = srv.overworld().getDataStorage();
-            return storage.computeIfAbsent((tag) -> HomeData.load(tag), HomeData::new, NAME);
+            return storage.computeIfAbsent(TYPE, NAME);
         }
         public static HomeData load(CompoundTag tag) {
             HomeData d = new HomeData();
