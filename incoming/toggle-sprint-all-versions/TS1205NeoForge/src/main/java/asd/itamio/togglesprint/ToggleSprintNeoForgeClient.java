e31c72048bfee72d754e4fd1a933da702ce36289
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-19 21:20:51 +0800

    Fix NeoForge 1.20.5/1.20.6: use ClientTickEvent from neoforge.client.event

--- incoming/toggle-sprint-all-versions/TS1205NeoForge/src/main/java/asd/itamio/togglesprint/ToggleSprintNeoForgeClient.java
+++ incoming/toggle-sprint-all-versions/TS1205NeoForge/src/main/java/asd/itamio/togglesprint/ToggleSprintNeoForgeClient.java
@@ -1,16 +1,14 @@
 package asd.itamio.togglesprint;
 
 import net.minecraft.client.Minecraft;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 public final class ToggleSprintNeoForgeClient {
     private static final ToggleSprintController CONTROLLER = new ToggleSprintController();
 
     private ToggleSprintNeoForgeClient() {}
 
-    public static void onClientTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            CONTROLLER.onClientTick(Minecraft.getInstance());
-        }
+    public static void onClientTick(ClientTickEvent.Post event) {
+        CONTROLLER.onClientTick(Minecraft.getInstance());
     }
 }
