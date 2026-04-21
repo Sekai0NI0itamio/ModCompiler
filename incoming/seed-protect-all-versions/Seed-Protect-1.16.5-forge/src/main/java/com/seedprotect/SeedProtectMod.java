5e376a4210010cb488a370aeae6509d2eaca9d81
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-21 09:40:01 +0800

    Fix Seed Protect mod for Forge 1.16.5+ versions
    - Remove invalid @EventHandler import for Forge 1.16.5
    - Update event import from world to level package for Forge 1.19+
    - Fix source code generation for all failed Forge versions

--- incoming/seed-protect-all-versions/Seed-Protect-1.16.5-forge/src/main/java/com/seedprotect/SeedProtectMod.java
+++ incoming/seed-protect-all-versions/Seed-Protect-1.16.5-forge/src/main/java/com/seedprotect/SeedProtectMod.java
@@ -3,9 +3,6 @@ package com.seedprotect;
 import net.minecraftforge.event.world.BlockEvent.FarmlandTrampleEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @Mod(SeedProtectMod.MOD_ID)
