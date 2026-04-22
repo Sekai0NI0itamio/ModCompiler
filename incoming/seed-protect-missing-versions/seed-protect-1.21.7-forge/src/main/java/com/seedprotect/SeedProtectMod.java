ddb2329f9314034c866541d817cb3769ec2a5dad
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-22 11:20:51 +0800

    Add Seed Protect missing versions (12 targets: 6 Fabric 1.18-1.19, 6 Forge 1.21.6-1.21.11)

---
+++ incoming/seed-protect-missing-versions/seed-protect-1.21.7-forge/src/main/java/com/seedprotect/SeedProtectMod.java
@@ -0,0 +1,18 @@
+package com.seedprotect;
+
+import net.minecraftforge.event.world.BlockEvent.FarmlandTrampleEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("seedprotect")
+@Mod.EventBusSubscriber(modid = "seedprotect", bus = Mod.EventBusSubscriber.Bus.FORGE)
+public final class SeedProtectMod {
+    public static final String MOD_ID = "seedprotect";
+    public static final String NAME = "Seed Protect";
+    public static final String VERSION = "1.0.0";
+
+    @SubscribeEvent
+    public static void onFarmlandTrample(FarmlandTrampleEvent event) {
+        event.setCanceled(true);
+    }
+}
