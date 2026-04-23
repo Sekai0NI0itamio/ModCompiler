7399555c60844a18378b405ce1577ff06ebf8131
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-23 16:15:28 +0800

    Add vein miner all-versions bundle (26 targets: Forge/Fabric/NeoForge 1.8.9-1.21.11)

---
+++ incoming/veinminer-all-versions/VeinMiner-1.16.5-fabric/src/main/java/asd/itamio/veinminer/VeinMinerMod.java
@@ -0,0 +1,16 @@
+package asd.itamio.veinminer;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VeinMinerMod implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("veinminer");
+    public static VeinMinerConfig config = new VeinMinerConfig();
+
+    @Override
+    public void onInitialize() {
+        PlayerBlockBreakEvents.BEFORE.register(new VeinMinerHandler());
+    }
+}
---
+++ incoming/veinminer-all-versions/VeinMiner-1.20.1-forge/src/main/java/asd/itamio/veinminer/VeinMinerMod.java
@@ -0,0 +1,21 @@
+package asd.itamio.veinminer;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("veinminer")
+public class VeinMinerMod {
+    public static final String MODID = "veinminer";
+    public static VeinMinerConfig config = new VeinMinerConfig();
+
+    public VeinMinerMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(FMLCommonSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(new VeinMinerHandler());
+        MinecraftForge.EVENT_BUS.register(new VeinMinerKeyHandler());
+    }
+}
