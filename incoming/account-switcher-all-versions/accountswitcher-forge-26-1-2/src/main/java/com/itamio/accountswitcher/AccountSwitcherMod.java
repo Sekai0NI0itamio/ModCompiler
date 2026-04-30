8c7be67fbdd46fce23220eb0faacf86f2d9d801f
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-04-30 09:56:29 +0800

    Add Account Switcher all-versions bundle (41 missing targets: Fabric 1.16.5-26.x, NeoForge 1.20.2-26.x, Forge 1.12+26.1.2)

---
+++ incoming/account-switcher-all-versions/accountswitcher-forge-26-1-2/src/main/java/com/itamio/accountswitcher/AccountSwitcherMod.java
@@ -0,0 +1,41 @@
+package com.itamio.accountswitcher;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("{MOD_ID}")
+public class AccountSwitcherMod {{
+    public static final String MODID = "{MOD_ID}";
+    private static final Logger LOGGER = LogManager.getLogger("AccountSwitcher");
+    private ConfigHandler configHandler;
+    private static String currentAccount = "Unknown";
+
+    public AccountSwitcherMod() {{
+        ScreenEvent.Render.Post.BUS.addListener(this::onRenderScreen);
+        configHandler = new ConfigHandler();
+        configHandler.startMonitoring();
+        LOGGER.info("Account Switcher Mod v{MOD_VERSION} initialized");
+    }}
+
+    private void onRenderScreen(ScreenEvent.Render.Post event) {{
+        try {{
+            if (!(event.getScreen() instanceof TitleScreen)) return;
+            GuiGraphicsExtractor gg = event.getGuiGraphics();
+            net.minecraft.client.gui.Font font = Minecraft.getInstance().font;
+            String prefix = "Account: ";
+            String account = currentAccount;
+            gg.text(font, prefix, 10, 10, 0xFFFFFF);
+            gg.text(font, account, 10 + font.width(prefix), 10, 0x00FF00);
+        }} catch (Exception e) {{ LOGGER.error("Render error", e); }}
+    }}
+
+    public static Logger getLogger() {{ return LOGGER; }}
+    public static String getCurrentAccount() {{ return currentAccount; }}
+    public static void updateCurrentAccount(String account) {{ currentAccount = account; }}
+}}
