ff13a849deef879a71990dc8d7a155d5c1f1b408
Sekai0NI0itamio <72232@mail.ssis-suzhou.net>
2026-05-04 20:57:49 +0800

    Fix no-particles: reflection for Fabric 1.21-1.21.8, RenderLevelStageEvent for NeoForge 1.20.x

--- incoming/no-particles-all-versions/NP-1.21.2-1.21.8-fabric/src/client/java/asd/itamio/noparticles/NoParticlesMod.java
+++ incoming/no-particles-all-versions/NP-1.21.2-1.21.8-fabric/src/client/java/asd/itamio/noparticles/NoParticlesMod.java
@@ -5,17 +5,30 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
+import java.lang.reflect.Method;
 
 @Environment(EnvType.CLIENT)
 public class NoParticlesMod implements ClientModInitializer {
+    private static Method clearParticlesMethod = null;
+
+    private static void clearParticles(Minecraft client) {
+        try {
+            if (client.particleEngine == null) return;
+            if (clearParticlesMethod == null) {
+                clearParticlesMethod = client.particleEngine.getClass().getDeclaredMethod("clearParticles");
+                clearParticlesMethod.setAccessible(true);
+            }
+            clearParticlesMethod.invoke(client.particleEngine);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
-            if (client.particleEngine != null) {
-                client.particleEngine.clearParticles();
-            }
+            clearParticles(client);
         });
     }
 }
